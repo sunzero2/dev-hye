8aa1fc1708fda1e6e6fd0d9d750c4f1ce27d1d15
sunzero2 <sun_zero2@naver.com>
2020-07-02 07:06:48 +0900

    hyeyeong

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
-/* var index = 0;
-var title = document.getElementById('title').innerHTML.split("");
+let text = '안녕하세요 신입 웹개발자 이혜영입니다';
+let textArr = text.split("");
+let index = 0;
+let introduce = "";
+let landing_text = document.querySelector('.landing-text');
+let text_box = document.querySelector('#text-box');
+let width_index = 0;
+let width;
 
-setInterval(function() {
-    var textBox = document.querySelector('.title');
-    var text = textBox.innerHTML;
-
-    if(index >= title.length) {
-        index = 0;
-        text = "";
+let interval = setInterval(function() {
+    if(index >= textArr.length) {
+        clearInterval(interval);
+    } else if(index == 5) {
+        width_index = 0;
+        introduce = "";
+    } else {
+        width = 30 * width_index;
+        introduce = introduce += textArr[index];
+        landing_text.innerHTML = introduce;
     }
 
-    textBox.innerHTML = text += title[index];
+    text_box.style.width = width + 'px';
+
     index++;
-}, 300); */
+    width_index++;
+}, 400);
+
 let header = document.getElementById("header");
 
 window.onscroll = function() {
-    if(window.scrollY >= 108) {
-        header.style.background = '#272822';
-        header.style.color = '#ececec';
+    if(window.scrollY >= 864) {
+        header.style.opacity = 1;
     } else {
-        header.style.background = 'white';
-        header.style.color = '#272822';
+        header.style.opacity = 0;
     }
 }

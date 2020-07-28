017a92ac60b8666e5c567c1424fc8079a88da664
sunzero2 <sun_zero2@naver.com>
2020-07-29 03:03:18 +0900

    디자인 변경

--- resources/index.js
+++ resources/index.js
@@ -1,43 +1,47 @@
-let text = '안녕하세요 신입 개발자 이혜영입니다';
-let textArr = text.split("");
-let index = 0;
-let introduce = "";
-let landing_text = document.querySelector('.landing-text');
-let text_box = document.querySelector('#text-box');
-let width_index = 0;
-let width;
+// const introduceText = '안녕하세요 신입 개발자 이혜영입니다';
+// const introduceTextSplitArray = introduceText.split("");
+// let introduceTextConcat = '';
+// let index = 0;
+// const landingText = document.querySelector('.landing-text');
+// const textBox = document.querySelector('#text-box');
+// let widthIndex = 0;
+// let width;
 
-let interval = setInterval(function() {
-    if(index >= textArr.length) {
-        clearInterval(interval);
-        document.querySelector('.scroll').style.display = 'block';
-    } else if(index == 5) {
-        width_index = 0;
-        introduce = "";
-    } else {
-        if(screen.width < 1030) {
-            width = 3.8 * width_index;
-        } else {
-            width = 2.4 * width_index;
-        }
+// // 안녕하세요 까지 출력할 경우 3초 뒤에 introduceTextPrint 함수 실행
+// // 그렇지 않을 경우 바로 introduceTextPrint 함수 실행
+// // index가 introduceTextSplitArray의 크기보다 클 경우 함수 실행하지 않음
+// let introduceTextPrint = function() {
+//     if(screen.width < 1030) {
+//         width = 3.8 * widthIndex;
+//     } else {
+//         width = 2.4 * widthIndex;
+//     }
+//     introduceTextConcat = introduceTextConcat += introduceTextSplitArray[index];
+//     landingText.innerHTML = introduceTextConcat;
+// }
+// let introduceTextInterval = setInterval(function() {
+//     if(index >= textArr.length) { // 소개문이 다 출력될 경우
+//         clearInterval(interval);
+//         document.querySelector('.scroll').style.display = 'block';
+//     } else if(index == 5) {
+//         width_index = 0;
+//         introduce = "";
+//         setTimeout(introducePrint(), 7000);
+//     } else {
+//         introducePrint();
+//     }
+//     text_box.style.width = width + '%';
+//     index++;
+//     width_index++;
+// }, 300);
 
-        introduce = introduce += textArr[index];
-        landing_text.innerHTML = introduce;
-    }
-
-    text_box.style.width = width + '%';
-
-    index++;
-    width_index++;
-}, 400);
-
-let header = document.getElementById("header");
+let header = document.querySelector("header");
 
 window.onscroll = function() {
-    if(window.scrollY >= 864) {
-        header.style.opacity = 1;
-    } else {
+    if(window.scrollY >= 100) {
         header.style.opacity = 0;
+    } else {
+        header.style.opacity = 1;
     }
 }
 
@@ -75,16 +79,6 @@ document.querySelectorAll('.skill-circle').forEach(function(el) {
 
     el.addEventListener('mouseout', function() {
         el.children[1].style.transform = 'translateY(49%)';
-    })
-})
-
-document.querySelectorAll('.project-wrap').forEach(function(el) {
-    el.addEventListener('mouseover', function() {
-        el.children[0].children[0].style.opacity = 1;
-    })
-
-    el.addEventListener('mouseout', function() {
-        el.children[0].children[0].style.opacity = 0;
     })
 })
 
